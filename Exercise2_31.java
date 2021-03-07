1d6b40bb5a324859db61a91cb1bff18386ce5f49
Malakkhanim <70220517+Malakkhanim@users.noreply.github.com>
2021-03-07 22:28:55 +0400

    Add files via upload

---
+++ Exercise2_31.java
@@ -0,0 +1,45 @@
+public class Exercise2_31 {
+    // main method begins execution of Java application
+    public static void main(String[] args) 
+    {
+        // initialize variables
+        int num;	// number for square and cube calculation
+        
+        System.out.println("number"+"\t"+"square"+"\t"+"cube");
+        
+        num = 0;
+        System.out.printf("%d \t %d \t %d\n", (num), (num * num), (num * num * num));
+        
+        num = 1;
+        System.out.printf("%d \t %d \t %d\n", (num), (num * num), (num * num * num));
+        
+        num = 2;
+        System.out.printf("%d \t %d \t %d\n", (num), (num * num), (num * num * num));
+        
+        num = 3;
+        System.out.printf("%d \t %d \t %d\n", (num), (num * num), (num * num * num));
+        
+        num = 4;
+        System.out.printf("%d \t %d \t %d\n", (num), (num * num), (num * num * num));
+        
+        num = 5;
+        System.out.printf("%d \t %d \t %d\n", (num), (num * num), (num * num * num));
+        
+        num = 6;
+        System.out.printf("%d \t %d \t %d\n", (num), (num * num), (num * num * num));
+        
+        num = 7;
+        System.out.printf("%d \t %d \t %d\n", (num), (num * num), (num * num * num));
+        
+        num = 8;
+        System.out.printf("%d \t %d \t %d\n", (num), (num * num), (num * num * num));
+        
+        num = 9;
+        System.out.printf("%d \t %d \t %d\n", (num), (num * num), (num * num * num));
+        
+        num = 10;
+        System.out.printf("%d \t %d \t %d\n", (num), (num * num), (num * num * num));
+    
+    } // end method main
+    
+    } // end class

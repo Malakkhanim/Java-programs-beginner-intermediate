75af1678bbdca3d1f531de67be707fd75972bac5
Malakkhanim <70220517+Malakkhanim@users.noreply.github.com>
2021-03-12 13:27:06 +0400

    Add files via upload

---
+++ Exercise2_29.java
@@ -0,0 +1,13 @@
+public class Exercise2_29 {
+    public static void main(String[] args){
+            char[] cArr = {'A', 'B', 'C', 'a', 'b', 'c', '0', '1', '2', '$', '+', '/', ' '};
+    
+            for(char c : cArr){
+                printResults(c);
+            }
+        }
+        // print results
+        private static void printResults(char c){
+            System.out.printf("The character %c has the value of %d\n", c, ((int) c));
+        }
+    }
